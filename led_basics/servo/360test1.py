a6e160d591a7ac137d89d8292a76d2a9e599ab4b
ysandeep999 <ysandeep999@gmail.com>
2016-01-27 02:26:03 -0500

    playing with continious servo motor

--- led_basics/servo/360test1.py
+++ led_basics/servo/360test1.py
@@ -1,13 +1,14 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
 
+GPIO.setmode(GPIO.BCM)
+GPIO.warnings(False)
 GPIO.setup(22, GPIO.OUT)
 
 p = GPIO.PWM(22, 25)
 
-p.start(50)
+p.start(49)
 
 
 
@@ -17,12 +18,12 @@ try:
 		print("wait started")
                 time.sleep(2)
 		print("wait ended")
-#                p.ChangeDutyCycle(12)
-#                time.sleep(2)
+                p.ChangeDutyCycle(12)
+                time.sleep(2)
 #		p.ChangeDutyCycle(25) # added code
 #		time.sleep(1)	 # added
-   #             p.ChangeDutyCycle(2)
-    #            time.sleep(2)
+                p.ChangeDutyCycle(2)
+                time.sleep(2)
 
 except KeyboardInterrupt:
         GPIO.cleanup()
